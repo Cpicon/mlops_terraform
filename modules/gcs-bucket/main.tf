8837c412082ea591b82d8f065a9cd6b7aed691cd
ChristianP <c.picon@uniandes.edu.co>
2025-08-08 06:07:01 -0500

    fix: Remove accidentally committed local files
    - Remove TERRAFORM_TUTORIAL.md (local work file)
    - Remove modules/gcs-bucket/* (local work files)
    - Remove environments/dev/BIGQUERY_ACCESS_EXPLAINED.md (local work file)
    
    These files were accidentally added with git add -A instead of specific files.
    My apologies for this mistake.

--- modules/gcs-bucket/main.tf
+++
@@ -1,7 +0,0 @@
- resource "google_storage_bucket" "example_bucket" {
-   name     = var.bucket_name
-   location = var.region
-   force_destroy = true
-
-   labels = var.labels
- }
--- modules/gcs-bucket/outputs.tf
+++
@@ -1,4 +0,0 @@
- output "bucket_url" {
-   description = "The URL of the bucket"
-   value       = google_storage_bucket.example_bucket.url
- }
